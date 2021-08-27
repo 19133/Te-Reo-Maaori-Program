2889209c8089b12e9c03c6a4fc6d3f84f1c6f3ba
ashkir <82185473+19133@users.noreply.github.com>
2021-08-27 08:05:29 +0000

    Initial commit

---
+++ welcome_name.py
@@ -0,0 +1,6 @@
+# welcomes user and asks users name
+print("Welcome to the Te Reo Maaori quiz")
+# Asks user their name
+print("What is your amazing name?")
+name = input ( )
+print("Kia Ora " + name)
