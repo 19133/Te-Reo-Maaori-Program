f9085a781edb5a86a504bb12620d84916a3a31f5
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-27 09:27:11 +0000

    turned question_answer component into a function. I also added score = 0 and score + 1 if the user gets an answer correct but it doesn't seem to work.

--- question_answer.py
+++ question_answer.py
@@ -1,21 +1,62 @@
-# asks user whats red in Te Reo Maaori
-quiz_question = input("What is red in Te Reo Maaori?\n")
+score = 0
+
+# red question function
+def quiz_question(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "whero":
+        user_response = "whero"
+        return user_response 
+
+    else:
+      print("Incorrect, try again") 
+
+# ocean question function
+def quiz_question2(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "moana":
+        user_response = "moana"
+        return user_response 
+
+    else:
+      print("Incorrect, try again") 
+
+
+# main routine goes here
+
+# whats red in te reo maaori? 
+qna_whero = quiz_question ("What’s red in Te Reo Maaori?\n")
 
 # if whero, print correct
-if quiz_question == "whero":
-  print ("Correct")
+if qna_whero == "whero":
+  print("Correct")
+  # if correct, score goes up by 1
+  score + 1
+  print("Your score is now", score)
+  print()
 
-# else, print incorrect  
+# if not whero, print incorect
 else:
-  print("Incorrect")
+  print("Incorect, try again")
+  print()
 
-# asks user whats ocean in Te Reo Maaori
-quiz_question2 = input ("What’s ocean in Te Reo Maaori?\n")
+# whats ocean in te reo maaori? 
+qna_awa = quiz_question2 ("What’s ocean in Te Reo Maaori?\n")
 
-# if whero, print correct
-if quiz_question2 == "Moana":
-  print ("Correct")
+# if moana, print correct
+if qna_awa == "moana":
+  print("Correct")
+  # if correct, score goes up by 1
+  score + 1
+  print("Your score is now", score)
+  print()
 
-# else, print incorrect  
+# if not moana, print incorect
 else:
-  print ("Incorrect") 
+  print("Incorect, try again")
+  print()
