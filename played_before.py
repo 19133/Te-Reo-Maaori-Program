3d78a4437a38008b093449bb1da2addd1929cae4
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-27 08:20:10 +0000

    Turned the played_before component into a function and I also added .lower() so upper case letters inputs turn into lower case letters.  I also got rid of the variable exit_code and made the exit code an else-if in the function but the program doesn't end when the user types xxx.

--- played_before.py
+++ played_before.py
@@ -1,3 +1,27 @@
+
+# yes/no function
+def played_before(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "yes" or user_response  == "y":   
+        user_response = "yes"
+        return user_response
+      
+    elif user_response == "no" or user_response  == "n":
+        user_response = "no"
+        return user_response 
+
+    elif user_response == "xxx":
+      print("Thanks for playing the Te Reo Maaori quiz")
+      print("We hope to see you again")
+
+
+    else:
+      print("Please say yes or no") 
+      print("Or type xxx to quit") 
+
 # welcomes user and asks users name
 print("Welcome to the Te Reo Maaori quiz")
 # Asks user their name
@@ -6,21 +30,21 @@ name = input ( )
 print("Kia Ora " + name)
 
 #asks user if they have played the Te Reo Maaori quiz
-played_before = input ("Have you played the Te Reo Maori quiz " + name + "?" + "\n" )
+played_quiz = played_before ("Have you played the Te Reo Maori quiz " + name + "?" + "\n" )
 
 # if yes, program continues
-if played_before == "yes":
+if played_quiz == "yes":
   print("program continues")
   
 # if no, explains what the quiz is
-elif played_before == "no":
+elif played_quiz == "no":
   print("Thats ok")
   print("The Te Reo Maaori quiz is a program which tests and helps students enhance their Te Reo Maaori language skill")
 
-# if xxx, program should end
-elif exit_code == "xxx":
+# if xxx, program ends
+elif played_quiz == "xxx":
   print("Thanks for playing the Te Reo Maaori quiz")
-  print("end program")
+  print("We hope to see you again")
 
 # else, asks user to type yes or no or xxx to quit then the question repeats
 else:
