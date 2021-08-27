f2a21fa83a6b3f2261e676432627fa9c9902a5b3
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-27 09:16:31 +0000

    Added while True and break which loops the question if the user types something that falls into the else statement. The while True loops everything under it forever and the break breaks the loop which is why I put break under each difficulty. I didn't put break under the else because I want the question to repeat again, I didn't put break under the xxx because the program would of ended by then

--- choose_difficulty.py
+++ choose_difficulty.py
@@ -1,23 +1,28 @@
+# While True loops the questions and break breaks the loop. I didn't add break for the else because I want the question to loop and ask the user the question again.
+while True:
+  # asks user if they would like to play the quiz on easy, medium, or hard
+  user_difficulty = input("Would you like to play on easy, medium, or hard\n").lower()
 
-# asks user if they would like to play the quiz on easy, medium, or hard
-choose_difficulty = input("Would you like to play on easy, medium, or hard\n").lower()
+  # if easy, the user will be asked easy questions (Haven't made the questions yet)
+  if user_difficulty == "easy":
+    print ("program continues")
+    break
+    
+  # elif medium, the program will print medium difficulty questions
+  elif user_difficulty == "medium":
+    print ("program continues")
+    break
+    
+  # elif hard, the program will print hard difficulty questions
+  elif user_difficulty == "hard":
+    print ("program continues")
+    break
+    
+  # elif xxx,   
+  elif user_difficulty == "xxx":
+    print ("Thanks for playing")
+    exit()
 
-# if easy, the user will be asked easy questions (Haven't made the questions yet)
-if choose_difficulty == "easy":
-  print ("program continues")
-  
-# elif medium, the program will print medium difficulty questions
-elif choose_difficulty == "medium":
-  print ("program continues")
-  
-# elif hard, the program will print hard difficulty questions
-elif choose_difficulty == "hard":
-  print ("program continues")
-  
-elif choose_difficulty == "xxx":
-  print ("Thanks for playing")
-  exit()
-
-# else, asks user to type easy, medium, hard, or xxx to quit
-else:
-  print("please type easy, medium, or hard")
+  # else, asks user to type easy, medium, hard, or xxx to quit
+  else:
+    print("please type easy, medium, or hard")
