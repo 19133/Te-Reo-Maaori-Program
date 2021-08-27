049312c37d7d8ac2422d6f268537714a0374a95c
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-27 09:55:07 +0000

    put [ ] around the answers in the dictionary which allowed moana and b to be answers to the question. I also changed if answer.lower = question_answers[randomized_question] to if answer.lower() in Questions_answers[randomize_quesestion]: which is another reason why moana and b are now both considered as answers

--- question_answer.py
+++ question_answer.py
@@ -1,31 +1,33 @@
 import random
 
+
 # quiz questions 
 Questions_answers = {
-    "What is ocean in Te Reo Maaori? \n A. kātao\n B. Moana\n C. Awa \n D. Wai":("b", "moana"),
-    "What is red in Te Reo Maaori?  \n A. whero \n B. Red \n C. whrea \n D. rato" :("a", "whero"),
-    "If it is Ua it is what? \n A. Rainy \n B. Sunny \n C. hot \n D. 21 hours ":("a", "rainy")
+    "What is ocean in Te Reo Maaori? \n A. kātao\n B. Moana\n C. Awa \n D. Wai":["b", "moana"],
+    "What is red in Te Reo Maaori?  \n A. whero \n B. Red \n C. whrea \n D. rato" :["a", "whero"],
+    "If it is Ua it is what? \n A. Rainy \n B. Sunny \n C. hot \n D. 21 hours ":["a", "rainy"]
 }   
+
 score = 0
 question = list (Questions_answers.keys())
 #input answer here 
 while True:
     if not question:
         break
-    randomize_question = random.choice(question)
-    print(randomize_question)
+    randomize_quesestion = random.choice(question)
+    print(randomize_quesestion)
     while True:
         answer = input('Answer ' )
         # if correct, moves onto next question
-        if answer.lower() == Questions_answers[randomize_question]:
+        if answer.lower() in Questions_answers[randomize_quesestion]:
             print("Correct Answer")
             print ()
             # if correct, score goes up by 1
-            score += 1
+            score =+ 1
             print("total score is", score)
             print()
             break
         else:
             #if wrong, Asks the same question again
             print("Wrong Answer, try again")
-    question.remove(randomize_question)
+    question.remove(randomize_quesestion)
